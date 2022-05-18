bea1a7b4b7e3ed17b6c01adad2b7a4039784ff9a
Nicholas Adorni <nicholasadorni1643@gmail.com>
2022-05-18 19:10:01 -0300

    initial commit

---
+++ teste.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+#include <iomanip>
+
+
+
+using namespace std;
+
+int main(){
+    string numero = "7";
+    int num  = 7;
+    if(num == numero){
+        cout << "sim";
+    }
+}
---
+++ trabalho2.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string.h>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::stoi;
+using namespace std;
+
+struct Movies {
+    string title;
+    string rating;
+    string ratingLevel;
+    string ratingDescription;
+    string releaseYear;
+    string userRating;
+    string userRatingSize;
+};
+
+int main() {
+    Movies netflix[1002];
+    ifstream inMovies("netflix_all.csv", ios::in);
+
+    string title;
+    string rating;
+    string ratingLevel;
+    string ratingDescription;
+    string releaseYear;
+    string userRating;
+    string userRatingSize;
+
+    int i = 0;
+
+    while(!inMovies.eof()){
+        getline(inMovies,title,';');
+        getline(inMovies,rating,';');
+        getline(inMovies,ratingLevel,';');
+        getline(inMovies,ratingDescription,';');
+        getline(inMovies,releaseYear,';');
+        getline(inMovies,userRating,';');
+        getline(inMovies,userRatingSize,';');
+
+        netflix[i].title = title;
+        netflix[i].rating = rating;
+        netflix[i].ratingLevel = ratingLevel;
+        netflix[i].ratingDescription = ratingDescription;
+        netflix[i].releaseYear = releaseYear;
+        netflix[i].userRating = userRating;
+        netflix[i].userRatingSize = userRatingSize;
+
+        i++;
+    }
+
+    int year;
+    year = 2016;
+    int ratingCount;
+    int yearCount = 0;
+    string ratingComparator;
+    int ratingCount = 0;
+
+    while(year != 1950){
+        yearCount = 0;
+        for(int i = 1; i <= 1001; i++){
+            if(stoi(netflix[i].releaseYear) == year){
+                yearCount++;    
+            }
+        }
+        if(yearCount == 0){
+            continue;
+        }
+        else{
+            cout << "Ano: " << year << " Quantidade: " << yearCount << endl;
+            year--;
+        }
+    }
+
+}
