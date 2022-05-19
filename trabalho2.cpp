64e77087bce690b41e71a5f513d6c69010a51eef
Nicholas Adorni <nicholasadorni1643@gmail.com>
2022-05-19 15:04:45 -0300

    more content added and some comments

--- trabalho2.cpp
+++ trabalho2.cpp
@@ -15,6 +15,7 @@ struct Movies {
 };
 
 int main() {
+    //exercicio 1
     Movies netflix[1002];
     ifstream inMovies("netflix_all.csv", ios::in);
 
@@ -47,26 +48,53 @@ int main() {
 
         i++;
     }
+    //=========================================================
 
+    //exercicio 3
     int year;
     year = 2017;
     int yearCount = 0;
     int ratingCount = 0;
 
-    //exercicio 3
-    while(year != 1988){
+    while(year != 1975){
         yearCount = 0;
         for(int i = 1; i <= 1001; i++){
             if(stoi(netflix[i].releaseYear) == year) yearCount++;    
         }
-        if(yearCount == 0) continue;
-        else{
-            cout << "Ano: " << year << " Quantidade: " << yearCount << endl;
-            year--;
+        year--;
+        if(yearCount == 0){
+            continue;
         }
+        cout << "Ano: " << year << " Quantidade: " << yearCount << endl;
     }
     //=======================================================================
 
+    //exercicio 5
+    //contador de filmes violentos
+    int violenceCount = 0;
+    //contador de filmes com sexual content
+    int nsfwCount = 0;
+    year = 2017;
+    
+    //loop começando em 2017 (filme mais novo em registro) e sendo iterado ate chegar em 1975 (filme mais antigo regitrado)
+    while(year != 1975){
+        for(int i = 1; i <= 1001; i++){
+            if(stoi(netflix[i].releaseYear) == year){
+                //filmes violentos estao entre 90 e 100 de rating description
+                if(stoi(netflix[i].ratingDescription) >= 90 && stoi(netflix[i].ratingDescription) <= 100){
+                    violenceCount++;
+                //filmes com sexual content estao acima de 110 de rating description
+                }else if(stoi(netflix[i].ratingDescription) >= 110){
+                    nsfwCount++;
+                }
+            }
+        }
+        //iterador negativo
+        year--;
+    }
 
+    cout << "Filmes Violentos: " << violenceCount << " porcentagem: " << (100*violenceCount)/1000 << "%" << endl;
+    cout << "Filmes contendo sexual content: " << nsfwCount << " porcentagem: " << (100*nsfwCount)/1000 << "%" << endl;
+    //=========================================================================
 
 }
